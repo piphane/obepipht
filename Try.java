39bbbe9bd200ec4d74bc1d5a4645b0ff93b15e6f
piphane <kenosexy6@gmail.com>
2019-11-02 19:41:51 -0300

    primeiro parte teste do trabalho prog 1 interpretador

---
+++ Try.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+class Try{
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		// xs = programa[p].split();
+		// String linha;
+     //System.out.println(linha);
+
+      //m  br.close();
+		//liny nome da variavel linha trocada.
+		String liny = "i";
+		if (liny.charAt(0)== 'v'){
+
+		}
+		for (int i = 0; i < liny.length(); i++){
+			System.out.println(liny.charAt(i));
+		}
+
+
+		while(ler.hasNextLine()) {
+			String linha = ler.nextLine();
+			System.out.println("> "+linha);
+			//int a=2;
+			//int x=1;
+			//int b=3;
+			double a=2.00;
+			double x=65.00;
+			double b=3.00;
+			double c =31.00;
+			String k ="we are trying ";
+			System.out.println(x+k);
+			if (x>60.00 && x<66.00) {
+				System.out.println("we are trying in the lill way my brother");
+				
+			}
+			else {
+				System.out.println("just try aigain");
+
+			}
+
+			}
+		// int x=1;
+		// String k ="we are trying ";
+		// System.out.println(x+k);
+		// System.out.println("ola mundo");
+		// if (x<=22) {
+		// 	System.out.println(x);
+
+		// 		}
+			
+		// 	else {
+		// 		System.out.println("cette comparaison nest pas possible a faire");
+		// 	}
+		//VETORES**** imp	
+		/*int p[] = new int [15];	
+		 for( int i =0;  i < 15; i++){  
+        System.out.println ("Digite um valor para a posição "+i+ " do vetor");
+        p[i] = ler.nextInt();
+    }
+    */	//Numero primo
+    //public class Exercicio2 {
+	
+	/*public static void main(String[] args) {
+		int N;
+                int cont = 0; 
+		Scanner tec = new Scanner (System.in);
+		System.out.print("Digite um número maior do que zero: ");
+
+				do {
+					System.out.print("Digite um número válido: ");
+					N = tec.nextInt();
+				} while (N <= 0);
+
+			for(int i = 1 ; i <=N ; i++){
+                             cont++;
+                            }
+                      if(cont==2){
+System.out.println("O numero " + String.valueOf(N) + " eh primo");
+}
+		}
+	
+}*/
+
+
+
+	}
+	
+}
