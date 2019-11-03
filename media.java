1696707666df29da6c6af60db51f13afba4ec59c
piphane <kenosexy6@gmail.com>
2019-11-03 15:00:01 -0300

    media dos numeros primos

---
+++ media.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+ class Exercicio01 extends Try {
+    
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+    double a = 2.00;
+    double b = 3.00;
+    double c = 31.00;
+    double x = 65.00;
+    double media ;
+
+            media = (a + b + c + x) / 4;
+            System.out.println("A média dos numeros  sao " + media);
+            
+        }
+            
+ }  
