f63b9a0a4f5bdbf54977ad18528352eaea314e89
obens <obensonmaurice23@gmail.com>
2019-11-04 21:50:43 -0300

    teste

---
+++ MediaTeste.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+ class MediaTeste{
+     public static void main(String[] args) {
+ //extends Try {
+    Scanner ler = new Scanner(System.in);
+        // xs = programa[p].split();
+    //ler.(programa.txt);
+    String liny = "i";
+        if (liny.charAt(0)== 'v'){
+
+        }
+        for (int i = 0; i < liny.length(); i++){
+            System.out.println(liny.charAt(i));
+        }
+
+
+        while(ler.hasNextLine()) {
+            String linha = ler.nextLine();
+            System.out.println("> "+linha);
+
+    
+    //public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+    double a = 2.00;
+    double b = 3.00;
+   //double c = 31.00;
+   // double x = 65.00;
+    double media ;
+
+            media = (a + b ) / 2;
+            System.out.println("A média dos numeros  sao " + media);
+        }
+    }        
+ 
+}   
+
+
